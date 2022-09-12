0b32aa90df6b7e00684c9f9d6c9d8a1a639545ab
IoSIM1 <113214669+IoSIM1@users.noreply.github.com>
2022-09-12 13:10:29 +0200

    Advice generator app coding challenge

---
+++ advice-generator-app-main/script.js
@@ -0,0 +1,23 @@
+const adviceText = document.querySelector(".advice-text");
+const dice = document.querySelector(".dice-img");
+
+let min = 1;
+let max = 50;
+let random;
+const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+console.log(randomNumber);
+const URL = `https://api.quotable.io/random`;
+
+const fetchUrl = function () {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((quotes) => {
+      adviceText.textContent = quotes.content;
+    });
+};
+
+dice.addEventListener("click", function () {
+  fetchUrl();
+});
+fetchUrl();
