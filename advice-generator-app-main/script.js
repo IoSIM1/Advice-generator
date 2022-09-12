4236c482f71e26680566d067562f10c7f879e888
IoSIM1 <113214669+IoSIM1@users.noreply.github.com>
2022-09-12 13:12:40 +0200

    Update script.js

--- advice-generator-app-main/script.js
+++ advice-generator-app-main/script.js
@@ -5,8 +5,9 @@ let min = 1;
 let max = 50;
 let random;
 const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-*/
+
 console.log(randomNumber);
+*/
 const URL = `https://api.quotable.io/random`;
 
 const fetchUrl = function () {
