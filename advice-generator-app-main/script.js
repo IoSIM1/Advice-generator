0e0037e51169dfa74fcf2532197bd21634e1f043
IoSIM1 <113214669+IoSIM1@users.noreply.github.com>
2022-09-12 13:12:16 +0200

    Update script.js

--- advice-generator-app-main/script.js
+++ advice-generator-app-main/script.js
@@ -1,11 +1,11 @@
 const adviceText = document.querySelector(".advice-text");
 const dice = document.querySelector(".dice-img");
-
+/* Useless part of code I was thinking about using other API in which I would need this part
 let min = 1;
 let max = 50;
 let random;
 const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-
+*/
 console.log(randomNumber);
 const URL = `https://api.quotable.io/random`;
 
